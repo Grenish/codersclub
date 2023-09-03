f9e48d0f861a7b26d4092aa60c98fde8e35bf4bb
Grenish rai <ariestheracer@gmail.com>
2023-09-03 20:39:41 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cards = () => {
   return (
     <div>
-      <article className="hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]">
+      <article className="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]">
         <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
           <time datetime="2022-10-10" className="block text-xs text-gray-500">
             10th Oct 2022
