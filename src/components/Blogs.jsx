03a621170d016ba2a39cc32a5d4fd07df89d2938
Grenish rai <ariestheracer@gmail.com>
2023-09-03 16:54:10 +0530

    Blogs Bug Fixed

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
 import { blogArticles } from "../data/blogs";
-import BlogArticle from './BlogArticle';
-
+import BlogArticle from "./BlogArticle";
+import { SectionWrapper } from "../hoc";
 
 const Blogs = () => {
   return (
-    <div>
-      {blogArticles.map((article) => (
-        <BlogArticle key={article.id} article={article} />
-      ))}
-    </div>
-  )
-}
+    <SectionWrapper role="region" aria-label="Blog Section">
+      <h1 tabIndex="0" id="blogs-header">
+        Our Blogs
+      </h1>
+      <ul aria-labelledby="blogs-header" role="list">
+        {blogArticles.map((article) => (
+          <li key={article.id}>
+            <BlogArticle article={article} />
+          </li>
+        ))}
+      </ul>
+    </SectionWrapper>
+  );
+};
 
-export default Blogs
+export default Blogs;
