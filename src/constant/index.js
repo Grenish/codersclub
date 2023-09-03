7a3be4b7e429caefb5310090f75b70246a838a0c
Grenish rai <ariestheracer@gmail.com>
2023-09-03 16:54:44 +0530

    Added guthub links

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,66 +5,132 @@ export const members = [
     id: 1,
     name: "Ayush Baral",
     photoUrl: profile,
+    github: "https://github.com/codeayush-7",
   },
   {
     id: 2,
     name: "Anish",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 3,
     name: "Gagan Sarma",
     photoUrl: profile,
+    github: "https://github.com/Gags-1",
   },
   {
     id: 4,
     name: "Grenish Rai",
     photoUrl: profile,
+    github: "https://github.com/Grenish",
   },
   {
     id: 5,
     name: "Kunsaqng",
     photoUrl: profile,
+    github: "https://github.com/jojosCodeBase",
   },
   {
     id: 6,
     name: "Tasso",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 7,
     name: "Yash",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 8,
     name: "Rohan",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 9,
     name: "Lorem",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 10,
     name: "Lorem",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 11,
     name: "Lorem",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 12,
     name: "Lorem",
     photoUrl: profile,
+    github: "",
   },
   {
     id: 13,
     name: "Lorem",
     photoUrl: profile,
+    github: "",
   },
 ];
 
+export const coordinators = [
+  {
+    id: 1,
+    name: "Dipendra Gurung",
+    photoUrl: profile,
+  },
+];
+
+export const missionPoints = [
+  {
+    title: "Promote Learning",
+    description:
+      "We organize workshops, hackathons, and coding competitions to enhance your coding skills. Whether you want to learn the basics or dive into advanced topics, our events cater to all levels of expertise.",
+  },
+  {
+    title: "Foster Innovation",
+    description:
+      "Innovation is at the heart of what we do. We encourage our members to think outside the box, come up with groundbreaking ideas, and turn them into reality through coding.",
+  },
+  {
+    title: "Build a Community",
+    description:
+      "We believe in the power of collaboration. Coders Club is a place to network, share knowledge, and work on exciting projects with like-minded individuals.",
+  },
+  {
+    title: "Contribute to Society",
+    description:
+      "We emphasize the importance of using technology for good. Our projects often have a societal impact, and we strive to make a positive difference in the world through our coding skills.",
+  },
+];
+
+export const offer = [
+  {
+    title: "Workshops and Tutorials",
+    description:
+      "Whether it's mastering a new programming language, exploring data science, or understanding the intricacies of web development, our workshops and tutorials provide valuable hands-on experience.",
+  },
+  {
+    title: "Hackathons and Competitions",
+    description:
+      "Test your skills, work under pressure, and showcase your creativity in our hackathons and coding competitions. Win prizes, gain recognition, and make lasting memories.",
+  },
+  {
+    title: "Projects Collaboration",
+    description:
+      "Join forces with fellow members to work on exciting coding projects. From developing apps to solving real-world problems, our projects offer practical experience and a chance to make a meaningful impact.",
+  },
+  {
+    title: "Guest Talks and Industry Insights",
+    description:
+      "We invite industry experts to share their experiences, insights, and the latest trends in technology. Stay up-to-date with the ever-evolving world of coding and tech.",
+  },
+];
