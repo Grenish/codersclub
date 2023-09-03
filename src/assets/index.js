25248283b740c3278d7375eb4410721b2f371c78
Grenish rai <ariestheracer@gmail.com>
2023-09-03 16:53:25 +0530

    Added github and smit logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,6 @@
 import codersclubLogo from './codersclubLogo.svg';
 import profile from './profile.jpeg';
+import github from './github-logo.png';
+import smitlogo from './smitlogo.png';
 
-export { codersclubLogo, profile };
+export { codersclubLogo, profile, github, smitlogo };
