56bc23c5c1011322b7b111752fb8e036caae9e11
Grenish rai <ariestheracer@gmail.com>
2023-09-03 05:37:03 +0530

    test blog

---
+++ src/data/blogs.js
@@ -0,0 +1,19 @@
+export const blogArticles = [
+  {
+    id: 1,
+    title: "First Meeting (Summary)",
+    postedOn: "01-09-2023",
+    postedBy: "codersclub",
+    content: [
+      {
+        type: "paragraph",
+        text: "Hello World! This is paragraph 1",
+      },
+      {
+        type: "paragraph",
+        text: "Hello World! This is paragraph 2 with points",
+        subPoints: ["point 1", "point 2", "point 3"],
+      },
+    ],
+  },
+];
