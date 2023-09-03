a53e451ba98513cc3863412497e3a9e5638cb47e
Grenish rai <ariestheracer@gmail.com>
2023-09-03 16:52:55 +0530

    Added more routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Navbar, Cards, Home, Members } from "./components";
-import { Route, Routes } from "react-router-dom";
+import {
+  Navbar,
+  Home,
+  Members,
+  About,
+  Blogs,
+  BlogArticle, // Remember to import BlogArticle
+} from "./components";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -9,6 +16,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/members" element={<Members />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/article/:id" element={<BlogArticle />} />
       </Routes>
     </div>
   );
