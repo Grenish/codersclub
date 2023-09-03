eea3d925dc15c96e08078093b03dd71c77ed545a
Grenish rai <ariestheracer@gmail.com>
2023-09-03 05:37:18 +0530

    Section Wrapper

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SectionWrapper = ({children}) => {
+  return (
+    <div className=' w-full sm:px-16 px-6 sm:py-32 py-32 '>
+      <div className="w-11/12 md:w-3/4 xl:w-7/12 m-auto">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default SectionWrapper
