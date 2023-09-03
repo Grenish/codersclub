d55de7dc92c044f0fedbdd9bfcd06747875e7464
Grenish rai <ariestheracer@gmail.com>
2023-09-03 05:35:50 +0530

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SectionWrapper } from '../hoc'
+
+const Home = () => {
+  return (
+    <div>
+      <SectionWrapper>
+        <h1 className='text-4xl font-bold text-center'>Home</h1>
+      </SectionWrapper>
+    </div>
+  )
+}
+
+export default Home
