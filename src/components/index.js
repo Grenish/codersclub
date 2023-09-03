4f862574d0430fd2c474049456e59ec678c5998e
Grenish rai <ariestheracer@gmail.com>
2023-09-03 05:36:09 +0530

    Import/export of components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import BlogArticle from "./BlogArticle";
+import Blogs from "./Blogs";
+import Cards from "./Cards";
+import Home from "./Home";
+import Members from "./Members";
+import Navbar from "./Navbar";
+
+export { BlogArticle, Blogs, Navbar, Cards, Home, Members };
