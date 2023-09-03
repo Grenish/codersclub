f4bed7e203d97b42a5691129f7df8826195f877f
Grenish rai <ariestheracer@gmail.com>
2023-09-03 05:36:44 +0530

    member names

---
+++ src/constant/index.js
@@ -0,0 +1,70 @@
+import { profile } from "../assets";
+
+export const members = [
+  {
+    id: 1,
+    name: "Ayush Baral",
+    photoUrl: profile,
+  },
+  {
+    id: 2,
+    name: "Anish",
+    photoUrl: profile,
+  },
+  {
+    id: 3,
+    name: "Gagan Sarma",
+    photoUrl: profile,
+  },
+  {
+    id: 4,
+    name: "Grenish Rai",
+    photoUrl: profile,
+  },
+  {
+    id: 5,
+    name: "Kunsaqng",
+    photoUrl: profile,
+  },
+  {
+    id: 6,
+    name: "Tasso",
+    photoUrl: profile,
+  },
+  {
+    id: 7,
+    name: "Yash",
+    photoUrl: profile,
+  },
+  {
+    id: 8,
+    name: "Rohan",
+    photoUrl: profile,
+  },
+  {
+    id: 9,
+    name: "Lorem",
+    photoUrl: profile,
+  },
+  {
+    id: 10,
+    name: "Lorem",
+    photoUrl: profile,
+  },
+  {
+    id: 11,
+    name: "Lorem",
+    photoUrl: profile,
+  },
+  {
+    id: 12,
+    name: "Lorem",
+    photoUrl: profile,
+  },
+  {
+    id: 13,
+    name: "Lorem",
+    photoUrl: profile,
+  },
+];
+
