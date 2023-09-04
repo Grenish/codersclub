eea5f4cf16bd2ebdda9690977f4e08de8e98eef0
Grenish rai <ariestheracer@gmail.com>
2023-09-04 23:32:11 +0530

    removed unnecessary tags

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,9 +66,6 @@ const Home = () => {
 
             <div className="mt-4 flex flex-wrap gap-1">
               <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600">
-                {latestArticle.postedBy}
-              </span>
-              <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600">
                 {latestArticle.tags}
               </span>
             </div>
@@ -103,9 +100,6 @@ const Home = () => {
               </Link>
 
               <div className="mt-4 flex flex-wrap gap-1">
-                <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600">
-                  {article.postedBy}
-                </span>
                 <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600">
                   {article.tags}
                 </span>
