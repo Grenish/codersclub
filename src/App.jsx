89f4865a29114b576d78d1fadd52c08b5c1f7852
Grenish rai <ariestheracer@gmail.com>
2023-09-05 22:28:58 +0530

    Modified routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,35 @@ import {
   Contact,
   Articles,
 } from "./components";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AdminLogin, Dashboard } from "./auth";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/members" element={<Members />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/article/:id" element={<BlogArticle />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/articles" element={<Articles />} />
-      </Routes>
-    </div>
-  );
+  const location = useLocation();
+
+  const renderMainContent = () => {
+    if (location.pathname === "/admin") {
+      return <AdminLogin />;
+    } else {
+      return (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/members" element={<Members />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/article/:id" element={<BlogArticle />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      );
+    }
+  };
+
+  return <div className="App">{renderMainContent()}</div>;
 };
 
 export default App;
