c638d5359dcc89681e49eca6be6a68ef1836d39b
Grenish rai <ariestheracer@gmail.com>
2023-09-06 22:45:29 +0530

    update

--- client/src/auth/helper/supabaseClient.js
+++ client/src/auth/helper/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPABASE_PROJECT_URL,
-  process.env.SUPABASE_PROJECT_ANON_KEY
+  "https://iyicdecacsjmxiuqcmsa.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5aWNkZWNhY3NqbXhpdXFjbXNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MjQ5MDksImV4cCI6MjAwOTUwMDkwOX0.QTUg94fKnF_mkQhGpSAihwjPSUymvZ4Arx4jvl2UZ1o"
 );
